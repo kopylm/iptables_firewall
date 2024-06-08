ab2d138e7a0770b03f916e3c0f1d30bb625d8dd1
kopylm <kopylmv@gmail.com>
2024-06-08 23:23:29 +0200

    add logger, ip address check, fix issues and naming, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# iptables_firewall
+# Iptables Firewall
+The application is designed to change host INPUT iptable according to the rules in config file.
+
+## Overview
+**main.py** - iptables Firewall app <br />
+**logger_config.py** - logger object configuration <br />
+**requirements.txt** - required packages <br />
+**rules.json** - iptables rules configuration that need to be applied for a host <br />
+**nodes.json** - list of mocked nodes with metadata (Consul) <br />
+
+## Installation for testing
+1. Use the [Docker](https://docs.docker.com/reference/cli/docker/image/build/) to create a docker image.
+From application root dir run:
+```bash
+docker build .
+```
+2. Run [docker container](https://docs.docker.com/reference/cli/docker/container/run/) with ___--privileged___ to have 
+access to iptables inside container. If you want use mocked host ip address also need to add ___-e___ with any ip address 
+from **services.json**
+```bash
+docker run --privileged -e MOCKED_HOST_IP='10.10.0.17' IMAGE[:TAG]
+```
--- main.py
+++ main.py
@@ -1,29 +1,64 @@
 import json
 import requests
 import iptc
-import logging
+import socket
+import os
+from logger_config import setup_logger
 
+logger = setup_logger()
 
 CONSUL_CATALOG_URL = "http://localhost:8500/v1/catalog/service/wireguard"
-SERVICES_FILE = 'services.json'
+NODES_DATA_FILE = 'nodes.json'
 FIREWALL_RULES = 'rules.json'
 
 
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        s.connect(('8.8.8.8', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+
+def get_mocked_host_ip():
+    return os.environ['MOCKED_HOST_IP']
+
+
 def get_catalog_info():
-    data = requests.get(CONSUL_CATALOG_URL)
-    return data.json()
+    try:
+        data = requests.get(CONSUL_CATALOG_URL)
+        logger.info('Successfully fetched catalog info from the Consul')
+        return data.json()
+    except Exception as e:
+        logger.error(f"Error fetching catalog from the Consul: {e}")
+        return []
 
 
 def get_catalog_info_from_file():
-    with open(SERVICES_FILE) as services_file:
-        file_contents = services_file.read()
-    return json.loads(file_contents)
+    try:
+        with open(NODES_DATA_FILE) as services_file:
+            file_contents = services_file.read()
+            logger.info('Successfully fetched catalog info from the file')
+        return json.loads(file_contents)
+    except Exception as e:
+        logger.error(f"Error fetching catalog from the file: {e}")
+        return []
 
 
 def get_firewall_rules():
-    with open(FIREWALL_RULES) as rules_file:
-        file_contents = rules_file.read()
-    return json.loads(file_contents)
+    try:
+        with open(FIREWALL_RULES) as rules_file:
+            file_contents = rules_file.read()
+            logger.info('Loaded firewall rules')
+        return json.loads(file_contents)
+    except Exception as e:
+        logger.error(f"Error loading firewall rules: {e}")
+        return []
 
 
 # Get IPs of services that are allowed to initiate connections
@@ -38,28 +73,28 @@ def get_source_ips(data, fleets):
             for service in data:
                 if service['NodeMeta']['env'] == fleet:
                     source_ips.append(service['ServiceAddress'])
-
+    logger.debug(f"Source IPs: {source_ips}")
     return source_ips
 
 
 # Get IPs of services that are allowed to receive connections
-def get_target_ips(data, envs, stages):
-    target_ips = []
+def get_destination_ips(data, envs, stages):
+    destination_ips = []
 
     for stage in stages:
         for env in envs:
             for service in data:
                 if env == '*':
-                    target_ips.append(service['ServiceAddress'])
+                    destination_ips.append(service['ServiceAddress'])
                 else:
                     if service['NodeMeta']['env'] == env and service['NodeMeta']['stage'] == stage:
-                        target_ips.append(service['ServiceAddress'])
-
-    return target_ips
+                        destination_ips.append(service['ServiceAddress'])
+    logger.debug(f"Target IPs: {destination_ips}")
+    return destination_ips
 
 
 # Create iptables rules
-def generate_iptables_rules(data, rules):
+def generate_iptables_rules(data, rules, current_host_ip):
     result = []
 
     for rule in rules['rules']:
@@ -69,12 +104,13 @@ def generate_iptables_rules(data, rules):
         fleets = rule['fleets']
 
         source_ips = get_source_ips(data, fleets)
-        target_ips = get_target_ips(data, envs, stages)
+        target_ips = get_destination_ips(data, envs, stages)
 
         for target_ip in set(target_ips):
-            for source_ip in source_ips:
-                result.append((port, source_ip, target_ip))
-
+            if target_ip == current_host_ip:
+                for source_ip in source_ips:
+                    result.append((port, source_ip, target_ip))
+    logger.info(f"Generated firewall rules: {result}")
     return result
 
 
@@ -82,13 +118,16 @@ def get_existing_rules():
     table = iptc.Table(iptc.Table.FILTER)
     chain = iptc.Chain(table, "INPUT")
     existing = []
-
     for rule in chain.rules:
-        if rule.protocol == "tcp":
-            match = rule.get_match("tcp")
-            if match:
-                existing.append((int(match.dport), rule.src, rule.dst))
-
+        protocol = rule.protocol
+        if protocol in ["tcp", "udp"]:
+            for match in rule.matches:
+                if match.name == protocol:
+                    dport = int(match.dport)
+                    src = rule.src.split('/')[0]
+                    dst = rule.dst.split('/')[0]
+                    existing.append((protocol, dport, src, dst))
+    logger.info(f"Existing rules: {existing}")
     return existing
 
 
@@ -97,6 +136,7 @@ def validate_rules(new_rules, existing_rules):
     for rule in new_rules:
         if rule not in existing_rules:
             valid_rules.append(rule)
+    logger.info(f"Valid rules to apply: {valid_rules}")
     return valid_rules
 
 
@@ -104,7 +144,7 @@ def apply_firewall_rules(rules):
     table = iptc.Table(iptc.Table.FILTER)
     chain = iptc.Chain(table, "INPUT")
 
-    # Apply new rules
+    # Apply the new rules
     for port, source_ip, target_ip in rules:
         rule = iptc.Rule()
         rule.protocol = "tcp"
@@ -114,12 +154,31 @@ def apply_firewall_rules(rules):
         rule.dst = target_ip
         rule.target = iptc.Target(rule, "ACCEPT")
         chain.insert_rule(rule)
+        logger.info(f"Applied rule: {port} {source_ip} {target_ip}")
 
 
-if __name__ == "__main__":
-    info = get_catalog_info_from_file()  # change function if we need to get info from consul service
+def main(current_host_ip):
+    catalog_data = get_catalog_info_from_file()  # change function if we need to get info from consul service
+    if not catalog_data:
+        logger.error("No catalog data found")
+        exit(1)
+
     firewall_rules = get_firewall_rules()
-    generated_rules = generate_iptables_rules(info, firewall_rules)
+    if not firewall_rules:
+        logger.error("No firewall rules found")
+        exit(1)
+
+    generated_rules = generate_iptables_rules(catalog_data, firewall_rules, current_host_ip)
     exist_rules = get_existing_rules()
     validated_rules = validate_rules(generated_rules, exist_rules)
     apply_firewall_rules(validated_rules)
+
+
+if __name__ == "__main__":
+    host_ip = get_mocked_host_ip()
+    if not host_ip:
+        logger.error("Error getting host ip")
+
+    logger.info(f"Host IP: {host_ip}")
+    main(host_ip)
+
